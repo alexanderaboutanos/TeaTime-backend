f604365a54160c2f7a87264af06affcaa4616942
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2022-03-08 08:59:23 -0500

    0.2 - add server.js file and config.js file

---
+++ config.js
@@ -0,0 +1,32 @@
+/** @format */
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const PORT = +process.env.PORT || 3001;
+
+/** Use development database, testing database, or via env var, production database */
+function getDatabaseUri() {
+  return process.env.NODE_ENV === "test"
+    ? "teaTime_test"
+    : process.env.DATABASE_URL || "teaTime";
+}
+
+/** Speed up bcrypt during tests */
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("TeaTime Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database".yellow, getDatabaseUri());
+console.log("---");
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+};
---
+++ server.js
@@ -0,0 +1,8 @@
+/** @format */
+
+const app = require("./app");
+const { PORT } = require("./config");
+
+app.listen(PORT, function () {
+  console.log(`Started on http://localhost:${PORT}`);
+});

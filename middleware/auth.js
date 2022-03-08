d714b02d26724c266c35a6de9e4324bdb3717d39
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2022-03-08 15:36:04 -0500

    1.5 - added ensureLoggedIn middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const { UnauthorizedError } = require("../expressError");
+
+/** Middleware to use when they must be logged in.
+ *
+ * If not, raises Unauthorized.
+ */
+
+function ensureLoggedIn(req, res, next) {
+  try {
+    if (!res.locals.user) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+module.exports = ensureLoggedIn;
